bfa1f461a2a067fe12aa42aaeb878296492ccace
Louis Kirsch <louis@louiskirsch.com>
2018-02-27 10:14:33 +0000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='tfdatasets',
+    version='1.0',
+    packages=['tfdatasets'],
+    url='https://github.com/timediv/tfdatasets',
+    license='',
+    author='Louis Kirsch',
+    author_email='',
+    description=''
+)
